e09d41a20f94821d3fce48d533b9083d5f91b3d5
Liri Sokol <liri@apiiro.com>
2021-03-29 15:42:19 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# USAGE: ./install.sh
+# will install the latest tool executable to your /usr/local/bin
+
+pushd $TMPDIR
+  mkdir -p gitsnap
+  pushd gitsnap
+    distro=$(if [[ "`uname -s`" == "Darwin" ]]; then echo "osx"; else echo "linux"; fi)
+    curl -s https://api.github.com/repos/apiiro/git-snap/releases/latest | grep "browser_download_url.*$distro.zip" | cut -d : -f 2,3 | tr -d \" | xargs curl -sSL -o gitsnap.zip
+    unzip gitsnap.zip
+    chmod +x gitsnap-*
+    sudo cp -nf gitsnap-* /usr/local/bin/git-snap
+  popd
+  rm -rf gitsnap
+popd
